b4c7e97863606a86217e81b475a1fb796fd7336e
papaniaj <52494297+papaniaj@users.noreply.github.com>
2019-07-17 10:34:51 -0400

    Add files via upload

---
+++ assets/data/myClasses.ts
@@ -0,0 +1,8 @@
+import {ClassInfo} from '../../app/classinfo';
+
+export const MYCLASSES : ClassInfo[] =
+[
+    {cname:'SYST24444', room: 'G407',dow:"Wed",tod:"8-11"},
+    {cname:'PROG37721', room: 'G307',dow:"Wed",tod:"12-3"},
+    {cname:'DBAS32100', room: 'G207',dow:"Wed",tod:"4-7"}
+]
